336b1eabe8e71451682c43a5f29b52b57be69166
icegrave0391 <chuqiz@comp.nus.edu.sg>
2024-06-07 19:27:17 -0700

    x

--- CI-Examples/llama/run-tests-native.sh
+++ CI-Examples/llama/run-tests-native.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 source .env
-prompt_file=$(realpath `pwd`/prompt2.txt)
+prompt_file=$(realpath `pwd`/prompt.txt)
 result_file=$(realpath `pwd`/result.txt)
 log_file=$(realpath `pwd`/log)
 
 
 pushd llama.cpp
-    # ./main -m $MODEL -p "$prompt"
-    ./main -m $MODEL -f $prompt_file --log-enable --log-file $log_file > /dev/null
+    # ./main -m $MODEL -f $prompt_file --log-enable --log-file $log_file > /dev/null
+    ./main -m $MODEL -f $prompt_file -n 128
 popd
---
+++ CI-Examples/yolo/pre-req.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt install libopencv-dev
+sudo apt install vulkan-tools vulkan-validationlayers-dev libvulkan-dev
