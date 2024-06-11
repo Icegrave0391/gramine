4921328e800ca7a884ae4068a3f074d5d2cff255
Icegrave0391 <zhangchuqi1999@gmail.com>
2024-06-11 10:03:31 +0000

    x

--- CI-Examples/llama/run-tests.sh
+++ CI-Examples/llama/run-tests.sh
@@ -2,4 +2,6 @@
 
 source .env
 
-gramine-encos llama -m llama.cpp/llama-2-7b-chat.Q4_K_M.gguf -p $prompt
+test_prompt="how are you?"
+
+gramine-encos llama -m llama.cpp/llama-2-7b-chat.Q4_K_M.gguf -p "$test_prompt"
