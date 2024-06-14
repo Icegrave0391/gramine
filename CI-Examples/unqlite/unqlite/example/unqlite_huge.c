979728b5ef5e065f401586c89a8cd1b06381963a
Icegrave0391 <zhangchuqi1999@gmail.com>
2024-06-14 03:33:10 -0700

    x

--- CI-Examples/unqlite/unqlite/example/unqlite_huge.c
+++ CI-Examples/unqlite/unqlite/example/unqlite_huge.c
@@ -230,11 +230,11 @@ int main(int argc, char *argv[])
 	
 	t4 = clock();
 	double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
-	printf("Time taken  insert t1-t=%f, commit +fetch=%f, iterate=%f, all=%f\n", 
+	printf("Time taken  insert t1-t=%d, commit +fetch=%d, iterate=%d, all=%d\n", 
 		(double)(t1-t)/CLOCKS_PER_SEC, (double)(t2-t1)/CLOCKS_PER_SEC,
 		 (double)(t3-t2)/CLOCKS_PER_SEC, (double)(t3-t)/CLOCKS_PER_SEC);
 
-	printf("Time taken opendb=%f, closedb=%f, close-open=%f\n", 
+	printf("Time taken opendb=%d, closedb=%d, close-open=%d\n", 
 		(double)(t-t0)/CLOCKS_PER_SEC, (double)(t4-t0)/CLOCKS_PER_SEC);
 	return 0;
 }
