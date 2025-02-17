65a13465ce000609210fc0ed1499060ca2ef0397
icegrave0391 <chuqiz@comp.nus.edu.sg>
2025-02-17 07:31:08 -0700

    x

--- CI-Examples/llama/pre-req.sh
+++ CI-Examples/llama/pre-req.sh
@@ -6,12 +6,6 @@ pushd ../../../scripts
     source .env
 popd
 
-# update submodules
-#git submodule update --init --recursive
-
-# link llama2.cpp dir here
-ln -s $(realpath `pwd`/../../../llama.cpp/) llama.cpp
-
 # build llama2
 pushd llama.cpp
     make
@@ -20,7 +14,7 @@ popd
 # install model
 pushd llama.cpp
     if [ ! -f $MODEL ]; then
-        echo "Downloading $MODEL..."
-        wget https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_K_M.gguf
+        echo "Copying $MODEL..."
+        cp ~/llama-2-7b-chat.Q4_K_M.gguf ./
     fi
 popd
